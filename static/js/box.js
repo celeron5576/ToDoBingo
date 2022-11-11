ce6891556129e124e64d18620f77d78996417570
celeron5576 <celeron5576@gmail.com>
2022-11-11 11:39:02 +0900

    入力されたらinputからpに変える処理

--- static/js/box.js
+++ static/js/box.js
@@ -1,11 +1,12 @@
-for (let x = 0; x < box_num * box_num; x++){
+box_ids = []
+
+//id名を動的に生成して配列に追加
+for (let x = 0; x < box_num * box_num; x++) {
     var box_id_num = "input_box" + x;
     console.log(box_id_num);
-    let boxInput = document.getElementById(box_id_num);
-    boxInput.addEventListener('change',function() {
-        valueInput(box_id_num);
-    });
+    box_ids[x] = box_id_num;
 }
+
 
 //let boxInput = document.getElementById('input_box0');
 //boxInput.addEventListener('change', valueInput);
@@ -14,17 +15,17 @@ for (let x = 0; x < box_num * box_num; x++){
 
 //INPUT BOXを動的に生成するプログラム
 var box_num = 3;
-var box = '<div class="block">'
+var box = '<div class="block" id="block">'
 for (let n = 0; n < box_num; n++) {
     box += '<div>'
     box += '<div class="block_floor">'
     for (let m = 0; m < box_num; m++) {
-        box += '<div>'
+        box += '<div id="box_input_box' + (n * box_num + m) + '">'
         box += '<div class="box">'
-        if (n * box_num + m + 1 == (box_num * box_num - 1)/2+1){
-            box += '<input id="input_box'+( n * box_num + m )+'" class="input_box" name="input_box" type="text" value="FREE" placeholder="ToDoを入力">'
-        }else{
-            box += '<input id="input_box'+( n * box_num + m )+'" class="input_box" name="input_box" type="text" placeholder="ToDoを入力">'
+        if (n * box_num + m + 1 == (box_num * box_num - 1) / 2 + 1) {
+            box += '<input id="input_box' + (n * box_num + m) + '" class="input_box" name="input_box" type="text" value="FREE" placeholder="ToDoを入力">'
+        } else {
+            box += '<input id="input_box' + (n * box_num + m) + '" class="input_box" name="input_box" type="text" placeholder="ToDoを入力">'
         }
         box += '</div>'
         box += '</div>'
@@ -36,81 +37,69 @@ for (let n = 0; n < box_num; n++) {
 document.getElementById('wrap').innerHTML = box;
 
 
-function valueChange(){
+function valueChange() {
     var box_num = boxSelect.value
-    var box = '<div class="block">'
-for (let n = 0; n < box_num; n++) {
-    box += '<div>'
-    box += '<div class="block_floor">'
-    for (let m = 0; m < box_num; m++) {
+    var box = '<div class="block" id="block">'
+    for (let n = 0; n < box_num; n++) {
         box += '<div>'
-        box += '<div class="box">'
-        if (n * box_num + m + 1 == (box_num * box_num - 1)/2+1){
-            box += '<input id="input_box'+( n * box_num + m )+'" class="input_box" name="input_box" type="text" value="FREE" placeholder="ToDoを入力">'
-        }else{
-            box += '<input id="input_box'+( n * box_num + m )+'" class="input_box" name="input_box" type="text" placeholder="ToDoを入力">'
+        box += '<div class="block_floor">'
+        for (let m = 0; m < box_num; m++) {
+            box += '<div id="box_input_box' + (n * box_num + m) + '">'
+            box += '<div class="box">'
+            if (n * box_num + m + 1 == (box_num * box_num - 1) / 2 + 1) {
+                box += '<input id="input_box' + (n * box_num + m) + '" class="input_box" name="input_box" type="text" value="FREE" placeholder="ToDoを入力">'
+            } else {
+                box += '<input id="input_box' + (n * box_num + m) + '" class="input_box" name="input_box" type="text" placeholder="ToDoを入力">'
+            }
+            box += '</div>'
+            box += '</div>'
         }
         box += '</div>'
         box += '</div>'
     }
-    box += '</div>'
-    box += '</div>'
-}
-+ '</div>';
-document.getElementById('wrap').innerHTML = box;
+    + '</div>';
+    document.getElementById('wrap').innerHTML = box;
 
 
-//BOX INPUTに値が入っているかを判別する処理
-function valueInput(box_id_num) {
-    var box_input = document.getElementById(box_id_num).value
-    if(box_input != ""){
-        console.log(box_input)
+    for (let x = 0; x < box_num * box_num; x++) {
+        var box_id_num = "input_box" + x;
+        console.log(box_id_num);
+        box_ids[x] = box_id_num;
     }
-}
-
-for (let x = 0; x < box_num * box_num; x++){
-    var box_id_num = "input_box" + x;
-    console.log(box_id_num);
-    let boxInput = document.getElementById(box_id_num);
-    boxInput.addEventListener('change',function() {
-        valueInput(box_id_num);
-    });
-}
-
-//let boxInput = document.getElementById('input_box0');
-//boxInput.addEventListener('change', valueInput);
-
-for (let x = 0; x < box_num * box_num; x++){
-    var box_id_num = "input_box" + x;
-    let boxInput = document.getElementById(box_id_num);
-    boxInput.addEventListener('change',function() {
-        valueInput(box_id_num);
-    });
-}
-
+    
+    
+    for (var i = 0; i < box_num * box_num; i++) {
+        let boxInput = document.getElementById(box_ids[i])
+        boxInput.addEventListener('change', function () {
+            console.log(this.id);
+            console.log(this.value);
+            if (this.value != "") {
+                inputted_box = '<div class="inputtedbox">'
+                + '<p>'+this.value+'</p>'
+                + '</div>'
+                console.log(inputted_box)
+                document.getElementById("box_"+this.id).innerHTML = inputted_box;
+                console.log(this)
+            }
+        });
+    }
 }
 let boxSelect = document.getElementById('box');
 boxSelect.addEventListener('change', valueChange);
 
-
-
-//BOX INPUTに値が入っているかを判別する処理
-function valueInput(box_id_num) {
-    const box_input = document.getElementById(box_id_num).value
-    if(box_input != ""){
-        console.log(box_input)
-    }
-}
-
-/*
-for (let x = 0; x < box_num * box_num; x++){
+//id名を動的に生成して配列に追加
+for (let x = 0; x < box_num * box_num; x++) {
     var box_id_num = "input_box" + x;
     console.log(box_id_num);
-    let boxInput = document.getElementById(box_id_num);
-    boxInput.addEventListener('change',function() {
-        valueInput(box_id_num);
+    box_ids[x] = box_id_num;
+}
+
+//配列に入っているid名を使い、値が入った時の処理をする
+for (var i = 0; i < box_num * box_num; i++) {
+    let boxInput = document.getElementById(box_ids[i])
+    boxInput.addEventListener('change', function () {
+        if (this.value != "") {
+            console.log(this.value)
+        }
     });
 }
-*/
-let boxInput = document.getElementById(this);
-boxInput.addEventListener('change', valueInput);
