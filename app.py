ff887126426a8727b028d177c4a3dcde41b025f0
celeron5576 <celeron5576@gmail.com>
2022-12-20 22:30:06 +0900

    db作成のところまでできた

--- app.py
+++ app.py
@@ -1,17 +1,90 @@
 from flask import Flask,render_template, flash, redirect, request, session
 
+import sqlite3
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+con = sqlite3.connect('TestDB.db')
+# データベース作成
+
+
+#ユーザーテーブル
+# id: 主キー　連番
+# email: メールアドレス　NOT NULL
+# username: ユーザー名
+con.execute(
+    """CREATE TABLE Users(
+        ID INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        email STRING NOT NULL, 
+        password STRING NOT NULL,
+        username STRING
+        );
+        """)
+
+#ToDoテーブル
+# id: 主キー
+# title: タイトル
+# desciption: ToDoの内容
+# date: 制作日
+# contribution_id: 製作者id
+# like_num: いいね数
+con.execute(
+    """CREATE TABLE ToDos(
+        ID INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+        title STRING, 
+        description STRING, 
+        date INTEGER,
+        contribution_id INTEGER,
+        like_num INTEGER
+        );
+        """)
+
+#公開したToDoテーブル
+# id: 主キー
+# title: タイトル
+# desciption: ToDoの内容
+# date: 公開日
+# contribution_id: 製作者id
+# like_num: いいね数
+con.execute(
+    """CREATE TABLE Released_ToDos(
+        ID INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+        title STRING, 
+        description STRING, 
+        date INTEGER,
+        contribution_id INTEGER,
+        like_num INTEGER
+        );
+        """)
+
+
+#いいねテーブル
+con.execute(
+    """CREATE TABLE likes(
+        ID INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+        contribution_id INTEGER,
+        user_id STRING,
+        date INTEGER
+        );
+        """)
+
+
+
+
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
 
-@app.route('/index')
-def index():
-    return '<h1>Hello</h1>'
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 
